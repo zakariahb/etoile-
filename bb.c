d2c58841918a25d4afb4cb5e1e512ce18c89c1b2
zakariahb <laksyarz@gmail.com>
2024-02-29 18:47:08 +0000

    ewjf

---
+++ bb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main ()
+{
+    int i , j;
+
+    i = 1;
+    while( i <= 5)
+    {
+        j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    i = 4
+    while (i >= 1)
+    {
+        j = 1;
+        while(j <= i)
+        {
+            printf("*");
+            j--;
+        }
+        printf("\n");
+        i--;
+    }
+}
